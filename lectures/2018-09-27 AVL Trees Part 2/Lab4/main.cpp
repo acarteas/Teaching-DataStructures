547e93251e4f834c5e95fa9f16f4db033ad13813
Adam Carter <mail@adam-carter.com>
2018-09-27 16:18:55 -0700

    uploaded final lecture code.

--- lectures/2018-09-27 AVL Trees Part 2/Lab4/main.cpp
+++ lectures/2018-09-27 AVL Trees Part 2/Lab4/main.cpp
@@ -11,13 +11,108 @@ public:
 	int value;
 	BinaryNode* left = nullptr;
 	BinaryNode* right = nullptr;
+    int height = 0;
 
-	BinaryNode(int v = 0)
+	BinaryNode(int v = 0, int h = 0)
 	{
 		value = v;
+        height = h;
 	}
 
+    int getBalanceFactor()
+    {
+        //BF is RH - LH
+        int right_height = 0;
+        int left_height = 0;
+        if (right != nullptr)
+        {
+            right_height = right->height;
+        }
+        if (left != nullptr)
+        {
+            left_height = left->height;
+        }
+        return right_height - left_height;
+    }
+
 };
+
+BinaryNode* rotateRight(BinaryNode* original_root)
+{
+    if (original_root == nullptr)
+    {
+        return original_root;
+    }
+    BinaryNode* new_root = original_root->left;
+    original_root->left = new_root->right;
+    new_root->right = original_root;
+
+    //recalculate heights of each
+    original_root = setHeight(original_root);
+    new_root = setHeight(new_root);
+    return new_root;
+
+}
+
+BinaryNode* balance(BinaryNode* node)
+{
+    if (node == nullptr)
+    {
+        return node;
+    }
+
+    //Node NEEDS to be balanced
+    //What direction do we need to rotate
+    if (node->getBalanceFactor() < 0)
+    {
+        //right rotation
+        if (node->left != nullptr && node->left->getBalanceFactor() > 0)
+        {
+            //rotate left at NR
+        }
+
+        //rotate right at OR
+    }
+    else
+    {
+        //left rotation
+        if (node->right != nullptr && node->right->getBalanceFactor() < 0)
+        {
+            //rotate right at NR
+        }
+        //rotate left at OR
+    }
+}
+
+BinaryNode* setHeight(BinaryNode* node)
+{
+    if (node == nullptr)
+    {
+        return node;
+    }
+
+    int right_height = -1;
+    int left_height = -1;
+    if (node->right != nullptr)
+    {
+        right_height = node->right->height;
+    }
+    if (node->left != nullptr)
+    {
+        left_height = node->left->height;
+    }
+    int height = 1 + (left_height < right_height) ? right_height : left_height;
+    node->height = height;
+    
+
+    //check balance factor
+    int balance_factor = node->getBalanceFactor();
+    if (balance_factor > 1 || balance_factor < -1)
+    {
+        return balance(node);
+    }
+    return node;
+}
 
 BinaryNode* addElement(int value)
 {
@@ -40,7 +135,7 @@ BinaryNode* addElement(BinaryNode* node, int value)
     {
         node->right = addElement(node->right, value);
     }
-    return node;
+    return setHeight(node);
 }
 
 int main(void)
