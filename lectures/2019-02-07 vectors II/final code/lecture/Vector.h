551e8558e68eb1092615791e32a42fc2de690f6e
Adam Carter <mail@adam-carter.com>
2019-02-07 10:22:55 -0800

    added final lecture code

--- lectures/2019-02-07 vectors II/final code/lecture/Vector.h
+++ lectures/2019-02-07 vectors II/final code/lecture/Vector.h
@@ -3,19 +3,10 @@
 #define VECTOR_H
 #include "SimpleIterator.h"
 
-
 template <typename T>
 class Vector {
 
 private:
-
-   //disallow copy and move constructors
-   Vector(const Vector<T>& other) {};
-   Vector(const Vector<T>&& other) {};
-
-   //disallow copy and move operators
-   Vector<T>& operator=(const Vector<T>& other) {};
-   Vector<T>& operator=(const Vector<T>&& other) {};
 
    //determines if we are able to put a value at the requested location
    void checkCapacity(int requested_location)
@@ -70,6 +61,60 @@ public:
 	   delete[] _items;
    }
 
+   //copy constructor
+   Vector(const Vector<T>& other) 
+   {
+	   if (_items != nullptr)
+	   {
+		   delete[] _items;
+	   }
+	   _items = new T[other._physical_size];
+	   _physical_size = other._physical_size;
+	   _logical_size = 0;
+	   for (int i = 0; i < other._logical_size; i++)
+	   {
+		   push_back(other._items[i]);
+	   }
+   }
+
+   //move constructor
+   Vector(Vector<T>&& other)
+   {
+	   _physical_size = other._physical_size;
+	   _logical_size = other._logical_size;
+	   _items = other._items;
+	   other._items = nullptr;
+   }
+
+   //copy operator
+   Vector<T>& operator=(const Vector<T>& other)
+   {
+	   if (_items != nullptr)
+	   {
+		   delete[] _items;
+	   }
+	   _items = new T[other._physical_size];
+	   _physical_size = other._physical_size;
+	   _logical_size = 0;
+	   for (int i = 0; i < other._logical_size; i++)
+	   {
+		   push_back(other._items[i]);
+	   }
+	   return *this;
+   }
+   Vector<T>& operator=(Vector<T>&& other)
+   {
+	   if (_items != nullptr)
+	   {
+		   delete[] _items;
+	   }
+	   _physical_size = other._physical_size;
+	   _logical_size = other._logical_size;
+	   _items = other._items;
+	   other._items = nullptr;
+	   return *this;
+   }
+
    //Adds new item to end of vector
    void push_back(const T& value)
    {
@@ -90,19 +135,37 @@ public:
 	   return _items[index];
    }
 
-
    SimpleIterator<T> begin()
    {
-      return SimpleIterator<int>{_items};
+	   return SimpleIterator<T>{_items};
    }
-
 
    SimpleIterator<T> end()
-   { 
-      return SimpleIterator<int>(&_items[_logical_size]);
+   {
+	   return SimpleIterator<T>{&_items[_logical_size]};
    }
 
+   void erase(SimpleIterator<T> location)
+   {
+	   for (auto start = location; start != &*end() - 1; start++)
+	   {
+		   *start = *(start + 1);
+	   }
+	   _logical_size--;
+   }
 
+   static void swap(Vector<T>& v1, Vector<T>& v2)
+   {
+	   //old method using copy constructors (slow!)
+	   /*Vector<T> temp = v1;
+	   v1 = v2;
+	   v2 = temp;*/
+
+	   //C++ 11 method using move constructors (fast!)
+	   Vector<T> temp{static_cast<Vector<T>&&>(v1)};
+	   v1 = static_cast<Vector<T>&&>(v2);
+	   v2 = static_cast<Vector<T>&&>(temp);
+   }
 };
 
 #endif // !VECTOR_H
