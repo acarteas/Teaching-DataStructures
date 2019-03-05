c6d6aa811ed20c259084c1299b5f9ee0dd0540c7
Adam Carter <mail@adam-carter.com>
2019-03-04 21:04:12 -0800

    Added tomorrow's lecture materials.

---
+++ lectures/2019-03-04 binary search trees/code/code/BinaryNode.h
@@ -0,0 +1,64 @@
+#pragma once
+#ifndef BINARY_NODE_H
+#define BINARY_NODE_H
+
+template <typename T>
+class BinaryNode
+{
+private:
+	T _value;
+	BinaryNode* _left = nullptr;
+	BinaryNode* _right = nullptr;
+public:
+
+	//constructor
+	BinaryNode(const T& value)
+	{
+		_value = value;
+	}
+
+	//empty constructor
+	BinaryNode()
+	{
+		//no clue what _value will be
+	}
+
+	BinaryNode* getLeft()
+	{
+		return _left;
+	}
+
+	void setLeft(BinaryNode* left)
+	{
+		_left = left;
+	}
+
+	BinaryNode* getRight()
+	{
+		return _right;
+	}
+
+	void setRight(BinaryNode* right)
+	{
+		_right = right;
+	}
+
+	T& getValue()
+	{
+		return _value;
+	}
+
+	const T& getValue() const
+	{
+		return _value;
+	}
+
+	void setValue(const T& value)
+	{
+		_value = value;
+	}
+};
+
+
+
+#endif
